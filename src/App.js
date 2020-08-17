e2852c78ca709314a4925531ff89aa57ed390765
AlexDamiao86 <alex_maiabr@hotmail.com>
2020-08-17 00:13:29 -0300

    Application using react hooks

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import Countries from './components/countries/Countries';
 import Header from './components/header/Header';
 
-export default class App extends Component {
-  constructor() {
-    super();
+export default function App() {
+  const [allCountries, setAllCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [filteredPopulation, setFilteredPopulation] = useState(0);
+  const [userFilter, setUserFilter] = useState('');
 
-    this.state = {
-      allCountries: [],
-      filteredCountries: [],
-      filteredPopulation: 0,
-      filter: '',
+  // https://www.robinwieruch.de/react-hooks-fetch-data
+  useEffect(() => {
+    const getCountries = async () => {
+      const res = await fetch('https://restcountries.eu/rest/v2/all');
+      let allCountries = await res.json();
+
+      allCountries = allCountries.map(
+        ({ name, numericCode, flag, population }) => {
+          return {
+            id: numericCode,
+            name,
+            filterName: name.toLowerCase(),
+            flag,
+            population,
+          };
+        }
+      );
+
+      setAllCountries(allCountries);
+      setFilteredCountries(Object.assign([], allCountries));
     };
-  }
 
-  async componentDidMount() {
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const json = await res.json();
+    getCountries();
+  }, []);
 
-    const allCountries = json.map(({ name, numericCode, flag, population }) => {
-      return {
-        id: numericCode,
-        name,
-        filterName: name.toLowerCase(),
-        flag,
-        population,
-      };
-    });
-
-    const filteredPopulation = this.calculateTotalPopulationFrom(allCountries);
-
-    this.setState({
-      allCountries,
-      filteredCountries: Object.assign([], allCountries),
-      filteredPopulation,
-    });
-  }
-
-  calculateTotalPopulationFrom = (countries) => {
+  const calculateTotalPopulationFrom = (countries) => {
     const totalPopulation = countries.reduce((accumulator, current) => {
       return accumulator + current.population;
     }, 0);
@@ -45,45 +42,35 @@ export default class App extends Component {
     return totalPopulation;
   };
 
-  handleChangeFilter = (newText) => {
-    this.setState({
-      filter: newText,
-    });
+  const handleChangeFilter = (newText) => {
+    setUserFilter(newText);
 
     const filterLowerCase = newText.toLowerCase();
 
-    const filteredCountries = this.state.allCountries.filter((country) => {
+    const filteredCountries = allCountries.filter((country) => {
       return country.filterName.includes(filterLowerCase);
     });
 
-    const filteredPopulation = this.calculateTotalPopulationFrom(
-      filteredCountries
-    );
+    const filteredPopulation = calculateTotalPopulationFrom(filteredCountries);
 
-    this.setState({
-      filteredCountries,
-      filteredPopulation,
-    });
+    setFilteredCountries(filteredCountries);
+    setFilteredPopulation(filteredPopulation);
   };
 
-  render() {
-    const { filteredCountries, filter, filteredPopulation } = this.state;
+  return (
+    <div className="container">
+      <h1 style={styles.centeredTitle}>React Countries</h1>
 
-    return (
-      <div className="container">
-        <h1 style={styles.centeredTitle}>React Countries</h1>
+      <Header
+        filter={userFilter}
+        countryCount={filteredCountries.length}
+        totalPopulation={filteredPopulation}
+        onChangeFilter={handleChangeFilter}
+      />
 
-        <Header
-          filter={filter}
-          countryCount={filteredCountries.length}
-          totalPopulation={filteredPopulation}
-          onChangeFilter={this.handleChangeFilter}
-        />
-
-        <Countries countries={filteredCountries} />
-      </div>
-    );
-  }
+      <Countries countries={filteredCountries} />
+    </div>
+  );
 }
 
 const styles = {
